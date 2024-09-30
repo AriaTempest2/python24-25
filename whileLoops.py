361d558af561b9949053f046bbc37e8607367fec
AriaTempest2 <148793715+AriaTempest2@users.noreply.github.com>
2024-09-30 14:22:19 +0000

    weeeeee

---
+++ whileLoops.py
@@ -0,0 +1,14 @@
+'''
+Program: whileLoops.py
+Author: Gavin Hollinger
+'''
+
+theSum = 0.0
+data = input("Enter a number or just enter to quit: ")
+
+while data != "":
+    number = float(data)
+    theSum += number
+    data = input("Enter a number or just enter to quit: ")
+
+print("The sum of your numbers is", theSum)
